95bab350da0236494037bc5597191bdc45497104
Rico Schiekel <fire@downgra.de>
2010-01-14 19:51:38 +0100

    fix duplicated Component class

--- guppy/factory.py
+++ guppy/factory.py
@@ -2,13 +2,8 @@
 # vim:syntax=python:sw=4:ts=4:expandtab
 
 from broker import features
+from support import Component
 import traceback
-
-
-class Component(object):
-
-    def __init__(self, class_name):
-        self.class_name = class_name
 
 
 def load_code(path):
@@ -25,15 +20,10 @@ def load_code(path):
         raise ImportError("attribute '%s' not found in '%s':\n%s" % (attribute_name, module_name, traceback.format_exc()))
 
 
-def create_component(class_name):
-    cls = load_code(class_name)
-    return cls()
-
-
 def create(config):
     for key, value in config.iteritems():
-        if isinstance(value, Component):
-            component = create_component(value.class_name)
+        if isinstance(value, Component) and value._class_name:
+            component = load_code(value._class_name)()
             features.Provide(key, component)
         else:
             features.Provide(key, value)
--- guppy/support.py
+++ guppy/support.py
@@ -7,3 +7,6 @@ class Protocol(object):
 
 class Component(object):
     "Symbolic base class for components"
+
+    def __init__(self, class_name = None):
+        self._class_name = class_name
